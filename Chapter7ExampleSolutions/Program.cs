80786b123a21d1076ae20d118f458552139ecfc0
devAbbasAlasgarov <devAbbasAlasgarov@gmail.com>
2022-09-27 16:17:39 +0400

    ExampleSolutions

--- Chapter7ExampleSolutions/Program.cs
+++ Chapter7ExampleSolutions/Program.cs
@@ -15,8 +15,182 @@ namespace Chapter7ExampleSolutions
 
         public static void Main(string[] args)
         {
-            
+            // Iteration with "foreach" Loop
+            /*
+            string[] capitals = { "Sofia", "Washington", "London", "Paris" };
+
+            foreach(string capital in capitals)
+            {
+                Console.WriteLine(capital);
+            }
+            */
+
+            // Iteration through the array in a reverse order
+            /*
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            Console.Write("Reversed: ");
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+            */
+
+            // Iteration through Elements of an Array
+            // Example 2
+            /*
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            Console.Write("Output: ");
+            for(int i = 0; i< array.Length; i+=2)
+            {
+                array[i] = array[i] * array[i];
+                Console.Write(array[i] + " ");
+            }
+            */
+
+            // Example 1
+            /*
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.Write("Output: ");
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                // Declaring the number
+                array[i] = 2 * array[i];
+                // Print the number
+                Console.Write(array[i] + " ");
+            }
+            */
+
+            // Printing an Array to the Console
+            /*
+            string[] array = { "one", "two", "three", "four" };
+
+            for(int i = 0; i< array.Length; i++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", i, array[i]);
+            }
+            */
+
+            // Check for Symmetric Array
+            /*
+            Console.WriteLine("Enter a positive integer: ");
+            int numberOfElements = int.Parse(Console.ReadLine());
+
+            int[] array = new int[numberOfElements];
+
+            Console.WriteLine("Enter the values of the array: ");
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            bool symmetric = true;
+            for(int i = 0; i < array.Length/2; i++)
+            {
+                if (array[i] != array[numberOfElements - i - 1])
+                {
+                    symmetric = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Is symmetric? {0}", symmetric);
+            */
+
+            // Reading an Array from the Console
+            /*
+            int numberOfElements = int.Parse(Console.ReadLine());
+
+            int[] array = new int[numberOfElements];
+
+            for(int i = 0; i < numberOfElements; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.WriteLine("Element {0} is {1}", i+1, array[i]);
+            }
+            */
+
+            // ArrayReverse
+            // Solution 2
+            /*
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] reversedArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                reversedArray[array.Length - i - 1] = array[i];
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(reversedArray[i] + " ");
+            }
+            */
+
+            // Solution 1
+            /*
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] reversedArray = new int[array.Length];
+
+            for(int i = array.Length-1, j = 0; i >= 0; i--, j++)
+            {
+                reversedArray[i] = array[j];
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(" array : " + array[i] + "reversedArray : " + reversedArray[i]);
+            }
+            */
+
+
+
+            /*
+            int[] arr = new int[5];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+            */
+
+            /*
+            int[] mySecondArray = new int[6];
+            mySecondArray[1] = 1;
+            mySecondArray[5] = 5;
+            */
+
+            /*
+            In this case we allocate an array of seven elements of type string.
+            The type string is a reference type (object) and its values are
+            stored in the dynamic memory. The variable daysOfWeek is allocated
+            in the stack memory, and points to a section of the dynamic memory
+            containing the elements of the array. The type of each of these
+            seven elements is string, which itself points to a different
+            section of the dynamic memory, where the real value is stored. 
+            */
+            /*
+            string[] dayOfWeek =
+                { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                "Saturday", "Sunday"};
+            */
+
+            /*
+            int[] myFirstArray = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(myFirstArray[3]);
+            */
+
+            /*
+            int[] array = new int[6];
+            Console.WriteLine(array[4]);
+            */
+
             Console.Read();
+
         }
     }
 }
